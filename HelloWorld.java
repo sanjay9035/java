e4023a986d5c4a6fea1bb17578f6f62744a38e85
sanjay9035 <52272417+sanjay9035@users.noreply.github.com>
2019-11-21 12:22:36 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Welcome to Devops Class Ram, Jimmy,Sanjay");
+      System.out.println("Hello all, Welcome to Devops Class Bharath,Ram, Jimmy,Sanjay");
    }
 }
